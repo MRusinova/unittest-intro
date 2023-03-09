7304a5080e9a305f2b8c4a5e1b61f733fe8efc78
MRusinova <rusinovamihaela@gmail.com>
2023-03-09 10:49:27 +0000

    Refactor code

--- evens.py
+++ evens.py
@@ -1,4 +1,4 @@
-def even_number_of_evens(number):
+def even_number_of_evens(numbers):
     """
     Should raise a TypeError if a list is not passed into the
     error message: "A list was not passed into the function"
@@ -8,7 +8,19 @@ def even_number_of_evens(number):
     if the number of even numbers is even, return True
     """
 
-    return None
+    if isinstance(numbers, list):
+        evens = sum([1 for n in numbers if n % 2 ==0])
+
+        return True if evens and evens % 2 == 0 else False
+
+        #if evens:
+         #   return evens % 2 == 0
+        #else:
+         #   return False
+    else:
+        raise TypeError("A list was not passed into the function")
+
+
 
 if __name__ == "__main__":
-    print(even_number_of_evens(5))
+    even_number_of_evens([2, 1, 4])
